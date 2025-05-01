ae723c62ef04f0deb96073eeeeec2440275de4bc
BlakeCaraballo <blakecaraballo1@gmail.com>
2025-05-01 11:47:27 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <Container className="text-center mt-5">
+      <h1>♠️ Blackjack Game ♥️</h1>
+      <p>Let’s play some cards!</p>
+    </Container>
+  );
+};
+
+export default App;
